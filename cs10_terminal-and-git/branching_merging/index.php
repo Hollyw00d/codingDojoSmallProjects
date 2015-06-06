20f79fb1817acf8b43a724e82e3c0f211083785b
Hollyw00d <matt@mattjennings.net>
2015-06-05 17:09:06 -0700

    html and less skeleton created

---
+++ cs10_terminal-and-git/branching_merging/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Branching and Merging</title>
+    <link rel="stylesheet" type="text/css" href="less/styles.css" />
+</head>
+<body>
+
+    <h1>Test</h1>
+
+</body>
+</html>
