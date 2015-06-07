141f7f808af4fd0eddd138ab0b17f20e56358b07
Hollyw00d <matt@mattjennings.net>
2015-06-07 14:56:23 -0700

    h1 tag and paragraph text added

--- cs10_terminal-and-git/branching_merging/index.php
+++ cs10_terminal-and-git/branching_merging/index.php
@@ -2,12 +2,22 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Branching and Merging</title>
+    <title>Branching and Merging Assignment</title>
     <link rel="stylesheet" type="text/css" href="less/styles.css" />
 </head>
 <body>
 
-    <h1>Test</h1>
+    <div id="wrapper">
+
+        <h1>Coding Dojo Rocks!</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+
+
+
+
+    </div>
 
 </body>
 </html>
