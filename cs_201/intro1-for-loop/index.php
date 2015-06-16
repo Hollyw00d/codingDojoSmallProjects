583b2460f12b63b38beb58552aefc9e8cd4e51a8
Hollyw00d <matt@mattjennings.net>
2015-06-15 18:16:45 -0700

    intro 1 for loop php assgnmnt done

--- cs_201/intro1-for-loop/index.php
+++ cs_201/intro1-for-loop/index.php
@@ -1,7 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Intro 1 - PHP For Loop Assignment</title>
+</head>
+<body>
+
+<h3>Create a program that prints all the odd numbers from 1 to 1000. Use the for loop and don't use any array to do this exercise.</h3>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: mattpjennings
- * Date: 6/15/15
- * Time: 6:08 PM
- */
+function odd_num()
+{
+
+    for($i = 1; $i < 1001; $i++)
+    {
+        if($i % 2 != 0) {
+            echo $i . '<br />';
+        }
+    }
+
+}
+
+odd_num();
+?>
+
+</body>
+</html>
