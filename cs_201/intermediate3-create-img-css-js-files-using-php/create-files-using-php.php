4b61b52cdc3bb0da9f5e3cc4aee402ac04375dd7
Hollyw00d <matt@mattjennings.net>
2015-06-17 16:04:20 -0700

    create files using php assgnmnt done

---
+++ cs_201/intermediate3-create-img-css-js-files-using-php/create-files-using-php.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Intermediate 3 - Create Image/CSS/JS File Using PHP</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css.php" />
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="js/main.js.php"></script>
+
+</head>
+<body>
+
+<h1>Heading 1</h1>
+<p>I had no idea you could use PHP to create a CSS file or any other files than html!!!</p>
+
+</body>
+</html>
---
+++ cs_201/intermediate3-create-img-css-js-files-using-php/js/main.js.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-type: text/javascript');
+?>
+// Test code
+$(function() {
+    alert('35 x 2 = 70');
+});
