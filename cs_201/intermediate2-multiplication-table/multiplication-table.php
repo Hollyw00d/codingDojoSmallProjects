1d443989ef3622e94f1f097c0590e992681c5b6c
Hollyw00d <matt@mattjennings.net>
2015-06-17 11:37:50 -0700

    middle of multiplication table assgnmnt

---
+++ cs_201/intermediate2-multiplication-table/multiplication-table.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Intermediate 2 - Multiplication Table</title>
+
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid #000;
+            width: 20px;
+            height: 20px;
+            text-align: center;
+        }
+
+
+    </style>
+
+</head>
+<body>
+
+<h1>Intermediate 2 - Multiplication Table</h1>
+
+<?php
+function multiplication_table($max)
+{
+
+    for($i = 0; $i < $max; $i++)
+    {
+        
+    }
+
+}
+
+
+
+?>
+
+<table>
+    <tr>
+        <td>&nbsp;</td>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+    </tr>
+
+</table>
+
+</body>
+</html>
