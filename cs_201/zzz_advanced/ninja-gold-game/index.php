0c6cc7f73054730dac69c109e7182a79e7f637d1
Hollyw00d <matt@mattjennings.net>
2015-06-18 14:53:12 -0700

    ninja gold game assgnmnt done

--- cs_201/zzz_advanced/ninja-gold-game/index.php
+++ cs_201/zzz_advanced/ninja-gold-game/index.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+
+// Set default timezone as Pacific Time
+date_default_timezone_set('America/Los_Angeles');
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,15 +18,15 @@ session_start();
 
     <h1>Ninja Gold Game</h1>
 
-    <h2 id="your-gold-heading">You Gold: <span id="gold-counter">0</span></h2>
+    <h2 id="your-gold-heading">Your Gold:<span id="gold-counter"><?php if(isset($_SESSION['gold_count'])) { echo $_SESSION['gold_count'];} else { echo 0; } ?></span> </h2>
 
     <div id="games">
 
         <div class="game">
             <h3>Farm</h3>
             <p>(earns 10 &mdash; 20 golds)</p>
-            <form action="process.php">
-                <input type="hidden" name="farm_gold" value="farm_gold"/>
+            <form action="process.php" method="post">
+                <input type="hidden" name="building" value="farm"/>
                 <input type="submit" value="Find Gold!"/>
             </form>
         </div>
@@ -31,8 +34,8 @@ session_start();
         <div class="game">
             <h3>Cave</h3>
             <p>(earns 5 &mdash; 10 golds)</p>
-            <form action="process.php">
-                <input type="hidden" name="farm_find_gold" value="farm_find_gold"/>
+            <form action="process.php" method="post">
+                <input type="hidden" name="building" value="cave"/>
                 <input type="submit" value="Find Gold!"/>
             </form>
         </div>
@@ -40,8 +43,8 @@ session_start();
         <div class="game">
             <h3>House</h3>
             <p>(earns 2 &mdash; 5 golds)</p>
-            <form action="process.php">
-                <input type="hidden" name="farm_find_gold" value="farm_find_gold"/>
+            <form action="process.php" method="post">
+                <input type="hidden" name="building" value="house"/>
                 <input type="submit" value="Find Gold!"/>
             </form>
         </div>
@@ -49,8 +52,8 @@ session_start();
         <div class="game">
             <h3>Casino!</h3>
             <p>(earns/takes 0 &mdash; 50 gold)</p>
-            <form action="process.php">
-                <input type="hidden" name="farm_find_gold" value="farm_find_gold"/>
+            <form action="process.php" method="post">
+                <input type="hidden" name="building" value="casino"/>
                 <input type="submit" value="Find Gold!"/>
             </form>
         </div>
@@ -58,8 +61,18 @@ session_start();
     </div>
 
     <h3>Activities:</h3>
-    <div id="activities">
 
+    <div id="activities">
+        <?php
+        if(isset($_SESSION['game_messages']))
+        {
+
+            foreach(array_reverse($_SESSION['game_messages']) as $value)
+            {
+                echo $value . '<br />';
+            }
+        }
+        ?>
 
     </div>
 
--- cs_201/zzz_advanced/ninja-gold-game/process.php
+++ cs_201/zzz_advanced/ninja-gold-game/process.php
@@ -1,7 +1,76 @@
 <?php
 session_start();
 
+function get_gold($min, $max)
+{
+    return rand($min, $max);
+}
+
+/*
+If the gold_count session variable doesn't exist assign it to 0
+ */
+if(!isset($_SESSION['gold_count']))
+{
+    $_SESSION['gold_count'] = 0;
+}
+
+/*
+If game_messages session variable doesn't exist
+assign it to an empty array
+*/
+if(!isset($_SESSION['game_messages']))
+{
+    $_SESSION['game_messages'] = array();
+}
+
+if(isset($_POST['building']))
+{
+
+    $_SESSION['building_name'] = $_POST['building'];
+
+    $building = $_POST['building'];
+    $gold_count = 0;
+    $activity = array();
+    $font_color = 'green';
+    $eared_or_lost = 'earned';
+
+    switch($building)
+    {
+        /*
+        Line below same as:
+        $_POST['building'] = 'farm';
+        */
+        case 'farm':
+            $gold_count = get_gold(10, 20);
+            break;
+        case 'cave':
+            $gold_count = get_gold(5, 10);
+            break;
+        case 'house':
+            $gold_count = get_gold(2, 5);
+            break;
+        case 'casino':
+            $gold_count = get_gold(-50, 50);
+            // If you lose money at the casino show
+            // red text
+            if($gold_count < 0)
+            {
+                $font_color = 'red';
+                $eared_or_lost = 'lost';
+            }
+            break;
+    }
+
+    $_SESSION['gold_count'] += $gold_count;
+
+
+    $_SESSION['game_messages'][] =
+        '<span class="' . $font_color . '">You entered a ' . $building . ' and ' . $eared_or_lost . ' ' . $gold_count .' golds. '
+    . '(' . date('F jS Y g:ia') . ')</span>';
+
+}
 
 
 header('Location: index.php');
+die();
 ?>
