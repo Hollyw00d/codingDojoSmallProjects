341bc683451121a3e42098980740002350ffdd76
Hollyw00d <matt@mattjennings.net>
2015-06-18 10:31:49 -0700

    ninja gold game skeleton set

---
+++ cs_201/zzz_advanced/ninja-gold-game/ninja-gold-game.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+// Set random number so it won't change
+// when session open
+//if(isset($_SESSION['rand_num_session']))
+//{
+//    $_SESSION['rand_num_session'] = rand(1, 2);
+//}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Great Number Game Assignment</title>
+
+</head>
+<body>
+
+<h1>Welcome to the Great Number Game!</h1>
+
+<p>I am thinking of a number between 1 and 100.</p>
+
+<p class="hide-if-correct"><em>Take a guess!</em></p>
+
+<h3><?php
+if(isset($_SESSION['rand_num_session']) && $_SESSION['my_guess'] < $_SESSION['rand_num_session'])
+{
+    echo 'You are too low!';
+}
+elseif(isset($_SESSION['rand_num_session']) && $_SESSION['my_guess'] > $_SESSION['rand_num_session'])
+{
+    echo 'You are too high!';
+}
+elseif(isset($_SESSION['rand_num_session']) && $_SESSION['my_guess'] == $_SESSION['rand_num_session'])
+{
+    echo 'You are correct and the random number is ' .
+$_SESSION['rand_num_session'] . '!!!';
+    echo '
+    <style type="text/css">
+        .hide-if-correct {
+            display: none;
+        }
+    </style>';
+}
+?></h3>
+
+<form class="hide-if-correct" action="processing.php" method="post">
+
+    <p><label for="guess"><input type="number" name="guess" placeholder="My Guess"/></label></p>
+
+    <p><label for="submit"><input type="submit" value="Submit"/></label></p>
+
+</form>
+
+<p><a href="reset.php">Reset Game</a></p>
+
+</body>
+</html>
