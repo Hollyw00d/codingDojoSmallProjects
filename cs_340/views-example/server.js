38a54627373e8364b051e04586ff2cef3324e46a
Hollyw00d <matt@mattjennings.net>
2015-07-14 13:56:06 -0700

    views example with express and ejs node modules

--- cs_340/views-example/server.js
+++ cs_340/views-example/server.js
@@ -6,6 +6,29 @@ app.get("/", function(req, res) {
     res.send("Hello Express!");
 });
 
+// Use express to display static files in a
+// "static" directory
+app.use(express.static(__dirname + "/static"));
+
+// Use express to display files that are server-side
+// dynamic in a "views" directory
+app.set("views", __dirname + "/views");
+
+// Set the view engine so that express knows we are using ejs
+app.set("view engine", "ejs");
+
+// Display users in a "users" route
+app.get("/users", function(req, res) {
+    // Hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"},
+        {name: "Jay", email: "jay@codingdojo.com"},
+        {name: "Brendan", email: "brendan@codingdojo.com"},
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    res.render("users", {users: users_array});
+});
+
 app.listen(8000, function() {
     console.log("Listening on port 8000");
 });
