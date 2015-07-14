7088d60648970fdabb8dca81cb2b4e63385d0ef1
Hollyw00d <matt@mattjennings.net>
2015-07-13 18:40:16 -0700

    first expressjs app

---
+++ cs_340/hello_express/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello Express!");
+});
+
+app.listen(8000, function() {
+    console.log("Listening on port 8000");
+});
