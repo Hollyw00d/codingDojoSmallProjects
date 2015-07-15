fe5948d7de6579d89050962285ece2d1d0e73d92
Hollyw00d <matt@mattjennings.net>
2015-07-15 12:28:37 -0700

    adding sockets to express lesson done

--- cs_340/adding-sockets-to-express/server.js
+++ cs_340/adding-sockets-to-express/server.js
@@ -2,20 +2,14 @@
 var express = require("express");
 var path = require("path");
 
-
-
 // Create the express app
 var app = express();
 
-// require http module and create a server variable to use later
-var http = require("http");
-var server = http.createServer(app);
-
 // Static content
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(path.join(__dirname + "/static")));
 
 // Setting up ejs and view dirs
-app.set("views", path.join(__dirname, "./views"));
+app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "ejs");
 
 // Root route to render the index.ejs view
@@ -23,17 +17,53 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
-// Tell express app to listen on port 8000
-app.listen(8000, function() {
-   console.log("Listening on port 8000");
-});
+// Express is listening port 8000
+var server = app.listen(8000);
 
 // We pass the server object
 var io = require('socket.io').listen(server);
 
 io.sockets.on("connection", function(socket) {
-
+    // This line console logs to
+    // Terminal
     console.log("I'm using sockets!");
+
+    // This line console logs to
+    // Terminal the "socket.id" value
     console.log(socket.id);
 
+    // This line executes when the
+    // socket.emit("button_clicked", ...)
+    // function executes from the
+    // client side and the "data"
+    // must be shown on the client side
+    // as an object
+    socket.on("button_clicked", function(data) {
+
+        // When the button click event is
+        // emitted from the client to
+        // the server this line
+        // console logs to Terminal AND
+        // the "response" property name
+        // matches the property name in
+        // the client
+        console.log("Someone clicked a button!\nReason: " + data.response);
+
+        // Also after the client side
+        // socket.emit("button_clicked",...)
+        // event executes the
+        // socket.emit("server_response")
+        // method executes and data from the
+        // "response" property is passed to
+        // the client
+        socket.emit("server_response", {response: "Sockets are the best!"});
+
+
+    });
+
+
+
 });
+
+
+
