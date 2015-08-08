333e4f64f9bb464a13689226eaf645b15aeb3e0b
Hollyw00d <matt@mattjennings.net>
2015-08-08 16:19:39 -0700

    redone active record using sinatra ruby framework

--- cs_330/sinatra/active-record/db/schema.rb
+++ cs_330/sinatra/active-record/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808212925) do
+ActiveRecord::Schema.define(version: 20150808223300) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
--- cs_330/sinatra/active-record/server.rb
+++ cs_330/sinatra/active-record/server.rb
@@ -2,15 +2,16 @@ require "sinatra"
 require "active_record"
 require "sqlite3"
 
-# "ActiveRecord" object has a "Base" class with
-# a "establish_connection" method that enables me to
-# establish a connection to a particular database
+# The "ActiveRecord" object has a "Base" class which in turn has a
+# "establish_connection" method which below creates a
+# "sqlite3" database connection and will eventually
+# create a database named "restful_routes_development.sqlite3" in the route directory
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "restful_routes_development.sqlite3"
 )
 
-# The "User" class inherits from the "ActiveRecord::Base" class
+# "User" class inherits from "ActiveRecord::Base"
 class User < ActiveRecord::Base
 end
 
