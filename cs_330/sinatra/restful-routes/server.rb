2d6af581f409148c73afd61e7a224de2080ac88c
Hollyw00d <matt@mattjennings.net>
2015-08-10 11:00:24 -0700

    CRUD operations on sinatra ruby demo done with login page too

--- cs_330/sinatra/restful-routes/server.rb
+++ cs_330/sinatra/restful-routes/server.rb
@@ -2,6 +2,9 @@ require "sinatra"
 require "active_record"
 require "sqlite3"
 
+# Enable sessions
+enable :sessions
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "restful_routes_development.sqlite3"
@@ -10,15 +13,57 @@ ActiveRecord::Base.establish_connection(
 class User < ActiveRecord::Base
 end
 
+# Home page with log in form
+get "/" do
+  # Form input name value to use later
+  # to create a session variable
+  erb :index
+end
+
+# Home page log in form
+post "/login" do
+  # 'params["password"]' refers to the text inputed
+  # inside the following tag on the "/index.erb" file:
+  # '<input type="password" name="password" />' and
+  # if the value submitted is "c0dingd0j0bellevue"
+  # created "loggedin" session variable and then
+  # redirect to the "/users" page
+  if params["password"] === "c0dingd0j0bellevue"
+    session["loggedin"] = "loggedin"
+    redirect "/users"
+  else
+    redirect "/"
+  end
+end
+
+# Log out form to destroy a session,
+# redirect to the home page,
+# and prevent peeps from visting any other page
+# until successfully logging in again on the home page
+post "/logout" do
+  # Set the session variable to "nil" and
+  # redirect to the home page
+  session["loggedin"] = nil
+  redirect "/"
+end
+
 # View all rows in "users" table
 get "/users" do
-  @users = User.all
-  erb :"users/index"
+  if !session["loggedin"]
+    redirect "/"
+  else
+    @users = User.all
+    erb :"users/index"
+  end
 end
 
 # Add a new row in "users" table
 get "/users/new" do
-  erb :"users/new"
+  if !session["loggedin"]
+    redirect "/"
+  else
+    erb :"users/new"
+  end
 end
 
 # Add a new user using the "Add a New User" form
@@ -30,14 +75,22 @@ end
 
 # Show a single user
 get "/users/:id" do
-  @user = User.find(params[:id])
-  erb :"users/show"
+  if !session["loggedin"]
+    redirect "/"
+  else
+    @user = User.find(params[:id])
+    erb :"users/show"
+  end
 end
 
 # Page to edit a single user
 get "/users/:id/edit" do
-  @user = User.find(params[:id])
-  erb :"users/edit"
+  if !session["loggedin"]
+    redirect "/"
+  else
+    @user = User.find(params[:id])
+    erb :"users/edit"
+  end
 end
 
 # "patch" connected to the
