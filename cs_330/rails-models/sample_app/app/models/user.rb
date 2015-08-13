990f2d0fcd9b4246af8498b2bd855497bb9f345e
Hollyw00d <matt@mattjennings.net>
2015-08-13 13:11:47 -0700

    rails associations lesson done

--- cs_330/rails-models/sample_app/app/models/user.rb
+++ cs_330/rails-models/sample_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :products
+
   # Valid email regex constant
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :first_name, :last_name, presence: true, length: { in: 2..20 }
--- cs_330/rails-models/sample_app/db/schema.rb
+++ cs_330/rails-models/sample_app/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813003639) do
+ActiveRecord::Schema.define(version: 20150813193705) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
