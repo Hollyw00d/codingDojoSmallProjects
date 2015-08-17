351b0d5a37fffd555244dc91ae2e68b6f9eac4c6
Hollyw00d <matt@mattjennings.net>
2015-08-17 13:29:49 -0700

    rails form validation done

--- cs_330/rails-controllers-views/demo/app/controllers/products_controller.rb
+++ cs_330/rails-controllers-views/demo/app/controllers/products_controller.rb
@@ -12,14 +12,11 @@ class ProductsController < ApplicationController
 
   # Method to create an item into the DB
   def create
-
-
     @products = Product.all
 
     # Use the "product_params" private method to force the force to ONLY submit data
     # for "name" and "description" columns
     @product = Product.new(product_params)
-    # @product = Product.new(name: params[:name], description: params[:description])
 
     # Line below allows me to view data submitted to Active Record DB
     # in source code after form is submitted AND fields aren't empty
@@ -29,7 +26,6 @@ class ProductsController < ApplicationController
       @errors = @product.errors.full_messages
       render "index"
     end
-
   end
 
   # Private method to force peeps to submit form that ONLY submits data for
