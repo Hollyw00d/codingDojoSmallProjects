dd575a420422850ce5108bd00a73f51afaeae1d8
Hollyw00d <matt@mattjennings.net>
2015-08-20 14:07:56 -0700

    product IV rails - assgnmnt done w/ some of rspec testing completed

--- cs_330/rails-controllers-views/product_management4/app/models/comment.rb
+++ cs_330/rails-controllers-views/product_management4/app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates :comment, :product_id, presence: true
+
   belongs_to :product
 end
--- cs_330/rails-controllers-views/product_management4/spec/models/comment_spec.rb
+++ cs_330/rails-controllers-views/product_management4/spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "requires a comment" do
+    newComment = Comment.new(comment: "")
+    newComment.valid?
+    expect(newComment.errors[:comment].any?).to eq(true)
+  end
+
+  describe "relationship" do
+    it "belongs to a product" do
+      comment = Comment.new
+      product = Product.new
+      product.comments << comment
+      expect(comment.product).to be(product)
+    end
+  end
 end
