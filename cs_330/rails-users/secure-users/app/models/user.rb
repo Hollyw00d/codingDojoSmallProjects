05fbca69b7ec57be611b5810a47fc420bd82ea28
Hollyw00d <matt@mattjennings.net>
2015-08-24 10:27:29 -0700

    rails users --> user assgnmnt done

--- cs_330/rails-users/secure-users/app/models/user.rb
+++ cs_330/rails-users/secure-users/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+
+  # Valid email regex constant
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: EMAIL_REGEX}
+
+  validates :password, presence: true
 end
